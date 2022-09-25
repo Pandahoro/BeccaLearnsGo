83736c7573df15cb909c75890820b645ccf51b20
Rebecca Neath <rebeccaneath@gmail.com>
2022-09-25 21:25:49 +0100

    Fixed swagger gen error and updated main.go

--- go-rest-api/internal/main.go
+++ go-rest-api/internal/main.go
@@ -2,18 +2,76 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/Pandahoro/BeccaLearnsGo/go-rest-api/pkg/swagger/server/restapi"
+
+	"github.com/Pandahoro/BeccaLearnsGo/go-rest-api/pkg/swagger/server/restapi/operations"
 )
 
 func main() {
+	//innit swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
 	
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
 
-	log.Println("Listening on localhost:8080")
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			//error handling
+			log.Fatalln(err)
+		}
+	}()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	server.Port = 8080
+
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHellowUser)
+
+	api.GetCatNameHandler = operations.GetCatNameHandlerFunc(GetCatByName)
+
+	//start server listening
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+
+	
+}
+
+//Health route returns OK
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+
+}
+
+//GetHelloUser returns Hello + your name
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+
+}
+
+//GetCatByName returns a cat in gif
+func GetCatByName(cat operations.GetCatNameParams) middleware.Responder {
+
+	var URL string
+	if cat.Name != ""{
+		URL = "https://github.com/Pandahoro/cats/raw/main" + cat.Name + ".gif"
+	} else {
+		// by defaut return sad cat
+		URL = "https://github.com/Pandahoro/cats/raw/main/SadCatto.gif"
+	}
+	
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	return operations.NewGetCatNameOK().WithPayload(response.Body)
 }
